fabdc460e90ebf42519408025336f30030cd8c4d
wimtheijken <info@theijken.nl>
2023-05-29 16:44:57 +0200

    correcties

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -5,7 +5,7 @@ function Fruit( { name, state, setState } ) {
     return (
         <article className={state === 0 ? "fruitmand-leeg" : "fruitmand-vol"}>
             <div><span className="fruit">{name}</span>
-                <button type="button" onClick={() => {state === 0 ? setState(0) : setState(state - 1)}}>-
+                <button type="button" disabled={state === 0} onClick={() => setState(state - 1)}>-
                 </button>
                 <span className="cijfer">{state}</span>
                 <button type="button" onClick={() => setState(state + 1)}>+</button>
